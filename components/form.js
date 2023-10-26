3bb8c2950b29e506ea93d15a622ffed9ae33459f
Julien <licornhizix@gmail.com>
2023-10-26 15:01:36 +0200

    create form component

---
+++ components/form.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const AddStaff= ()=> {
+
+
+    return (
+
+        <div>
+            <form>
+                <label>Full Name:</label>
+                <input></input>
+            </form>
+        </div>
+
+
+    )
+}
