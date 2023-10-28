b36e4e8d34c2860306e72bdd1f46b897f42b8348
Julien <licornhizix@gmail.com>
2023-10-28 16:54:11 +0200

    add app.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,16 @@
-const http = require('http');
+/*const http = require('http');
 
 const server = http.createServer((req, res) => {
     res.end('Ma Ksenia !');
 });
+
+server.listen(process.env.PORT || 3000);*/
+
+
+const http = require('http');
+const app = require('./app');
+
+app.set('port', process.env.PORT || 3000);
+const server = http.createServer(app);
 
 server.listen(process.env.PORT || 3000);
